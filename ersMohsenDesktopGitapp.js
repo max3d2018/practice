9759eca302ade4adad7e580ed79122b4f00541ff
max3d2018 <max3d2018@hotmail.com>
2024-11-14 03:35:31 -0600

    commited all

---
+++ ersMohsenDesktopGitapp.js
@@ -0,0 +1,17 @@
+[33mcommit 39db7fd6cc8497b91e6b603d90b9de49727bb4d2[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m
+Author: max3d2018 <max3d2018@hotmail.com>
+Date:   Thu Nov 14 03:32:06 2024 -0600
+
+    add Event listener to the button
+
+[33mcommit 2c19ced7103d1a6ec5a1fb18c49435970bc3a073[m
+Author: max3d2018 <max3d2018@hotmail.com>
+Date:   Thu Nov 14 03:26:35 2024 -0600
+
+    Added index.html file and filled with basics
+
+[33mcommit d17e882eb8f914de4c1f9823fabcbcf4692f2671[m
+Author: max3d2018 <max3d2018@hotmail.com>
+Date:   Thu Nov 14 03:19:31 2024 -0600
+
+    added app.js but Empty
